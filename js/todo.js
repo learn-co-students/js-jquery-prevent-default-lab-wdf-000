839c2c2abe0252d1ef602af53bdc28f33ec71430
irevived1 <haifeng.wu@jjay.cuny.edu>
2016-09-06 21:31:54 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
   // call functions here
+  submitForm();
 });
 
 // define functions here
+
+function submitForm(argument) {
+  $('form').submit(function (e) {
+    $('#list ol').append(`<li>${$('input#item').val()}</li>`);
+    e.preventDefault();
+  });
+
+}
+
