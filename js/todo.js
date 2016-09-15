2255453fce26b32b28a710e88ea75391277ac8b2
Raycent <raycent.tan@flatironschool.com>
2016-09-14 20:26:48 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
   // call functions here
+  submitForm();
 });
 
 // define functions here
+function submitForm(){
+  $('form').on('submit', handleFormSubmit)
+}
+
+function handleFormSubmit(event) {
+  $("#list ol").append('<li>' + $('#item').val() + '</li>');
+  event.preventDefault();
+}
