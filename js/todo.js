77beea9467f1e1f4f44ed0d7e99c504d160b8d1d
aehmt <calisahmet@gmail.com>
2016-09-30 13:01:02 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
   // call functions here
+  submitForm()
 });
 
 // define functions here
+function submitForm() {
+  $('form').on('submit', function(event){
+    var item = $('#item').val();
+    $("#list ol").append("<li></li>");
+    $("#list ol li:last").text(item)
+    event.preventDefault();
+  });
+}
