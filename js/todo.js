62cb8661bd7be6bd9140a01bd9d9e481a1c28c87
Derick Quartey <niikwartey1@gmail.com>
2016-09-30 10:47:54 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,22 @@
 $(document).ready(function(){
   // call functions here
+  submitForm();
 });
 
 // define functions here
+function submitForm() {
+
+  $('form').on('submit', function(event) {
+    event.preventDefault()
+
+    var todo = $("input#item").val();
+    addItem(todo)
+    
+  })
+}
+
+function addItem(todo) {
+  var ol = $('div#list ol')[0];
+  var li = $("<li></li>").text(todo);
+  $('div#list ol').append(li)
+}
