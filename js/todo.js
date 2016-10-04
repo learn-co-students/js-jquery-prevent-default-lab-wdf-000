09e56ad9aed5b218d25292b4b91b3868d04737f8
satub <satu.barnhill@gmail.com>
2016-10-04 16:32:45 -0400

    page works, tests do not

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
-  // call functions here
+  submitForm();
 });
 
-// define functions here
+function submitForm() {
+  $('form').on('submit', function(event){
+  var item = $('#item').val();
+  $("#list ol").append(`<li>${item}</li>`);
+  $('#item').val("");
+  event.preventDefault();
+  });
+}
