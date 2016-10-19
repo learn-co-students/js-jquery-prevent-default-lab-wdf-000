050cbc412e5b32924045418e66886c93cb012880
fidelscodes <fidelscodes@gmail.com>
2016-10-19 12:01:42 -0400

    Done

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
   // call functions here
+  submitForm();
 });
 
 // define functions here
+function submitForm() {
+  // NOTE: handleSubmit called wihtout () at the end returns the actual function itself
+  $('form').on('submit', handleSubmit);
+}
+
+function handleSubmit(event) {
+  const item = $('#item').val();
+  let liWithItem = `<li>${item}</li>`;
+
+  $('#list ol').append(liWithItem);
+  event.preventDefault();
+}
