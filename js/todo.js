3359fead28b0bc12a0ffe413509510c07a95f149
Bibi Khadiza <limakhadiza@yahoo.com>
2016-11-01 15:34:00 -0400

    done

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
   // call functions here
+  submitForm();
 });
 
 // define functions here
+function submitForm(){
+  $('form').on('submit', function(event){
+    var result = $('#item').val();
+    $("#list ol").append('<li>' + result + '</li>')
+    event.preventDefault();
+  });
+}
