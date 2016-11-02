05b15534b27fff1fe563c5752c21a34996b4855d
Wendy Xu <guangw.xu@gmail.com>
2016-11-02 18:02:37 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
   // call functions here
+  submitForm();
 });
 
 // define functions here
+
+function submitForm() {
+  $("form").on("submit", function(event) {
+    var list = $("input#item").val();
+    $("div#list ol").append(`<li>${list}</li>`); 
+    event.preventDefault();
+  });
+}
