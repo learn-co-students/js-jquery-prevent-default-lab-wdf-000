c3422311f0a8de116164044ef74c7615260f714b
Ekaterina Petushova <epetushova@gmail.com>
2017-02-01 12:45:01 -0500

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
   // call functions here
+  submitForm();
 });
 
 // define functions here
+function submitForm(){
+$('form').on('submit', function(event){
+	event.preventDefault();
+	var item = $('#item').val();
+	$('#list ol').append("<li>" + item + "</li>");
+	$('#item').val("")
+	});
+}
