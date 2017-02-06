604d9ee016739d514fd96559db7f2c2aecb6e011
dimitriwashere <dimitriwashere@gmail.com>
2017-02-06 13:53:22 -0500

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
-  // call functions here
+  submitForm();
 });
 
-// define functions here
+function submitForm() {
+  $('form').on('submit', function(event){
+    var toDoItem = $('#item').val();
+    $('ol').append('<li>' + toDoItem + '</li>');
+    event.preventDefault();
+  });
+}
